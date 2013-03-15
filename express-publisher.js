7c506254bf3cdb1ae7745de91b2dd08dc58ed128
Guillaume Charmetant <cguille.dev@gmail.com>
2013-03-15 09:05:42 +0100

    Initial commit

---
+++ express-publisher.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+(function () {
+    'use strict';
+    var express = require('express'),
+        app = express(),
+        appPort = +process.argv[2],
+        servingDirectory = __dirname + '/public';
+
+    if (isNaN(appPort)) {
+        appPort = 8080;
+    }
+
+    app.configure(function () {
+        app.use(express.compress());
+        app.use(express.logger('dev'));
+        app.use(express.static(servingDirectory));
+    });
+
+    var httpServer = app.listen(appPort, function () {
+        var binding = httpServer.address();
+        console.info('Server running on ' + binding.address + ':' + binding.port + ' (' + binding.family + ')');
+        console.info('Files will be served from "' + servingDirectory + '"');
+    });
+}());
+
