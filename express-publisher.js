d434c26fa8a12d977f9a92a86fa0ff9c81ae683f
Guillaume Charmetant <cguille.dev@gmail.com>
2013-03-15 09:44:28 +0100

    Add icons option to the directory middleware

--- express-publisher.js
+++ express-publisher.js
@@ -14,7 +14,7 @@
     app.configure(function () {
         app.use(express.compress());
         app.use(express.logger('dev'));
-        app.use(express.directory(servingDirectory));
+        app.use(express.directory(servingDirectory, { icons: true }));
         app.use(express.static(servingDirectory));
     });
 
